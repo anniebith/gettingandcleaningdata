a0ad1e7bf446d472c17ad6e526f0f71d9d6bd2d1
anniebith <41243442+anniebith@users.noreply.github.com>
2018-09-29 10:11:42 +1000

    Update and rename finalassignment to run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,140 @@
+packages <- c("data.table", "reshape2")
+sapply(packages, require, character.only = TRUE, quietly = TRUE)
+
+path <- getwd()
+path
+
+# #url <- "C:/Users/Ryan/Documents/gettingandcleaningdata"
+# #f <- "Dataset.zip"
+# if (!file.exists(path)) {
+#   dir.create(path)
+# }
+# download.file(url, file.path(path, f))
+
+
+# executable <- file.path("C:", "Program Files (x86)", "7-Zip", "7z.exe")
+# parameters <- "x"
+# cmd <- paste(paste0("\"", executable, "\""), parameters, paste0("\"", file.path(path, 
+#                                                                                 f), "\""))
+# system(cmd)
+# 
+pathIn <- file.path(path, "UCI HAR Dataset")
+list.files(pathIn, recursive = TRUE)
+
+#read the subject files
+
+dtSubjectTrain <- fread(file.path(pathIn, "train", "subject_train.txt"))
+dtSubjectTest <- fread(file.path(pathIn, "test", "subject_test.txt"))
+
+# Read the activity files
+dtActivityTrain <- fread(file.path(pathIn, "train", "Y_train.txt"))
+dtActivityTest <- fread(file.path(pathIn, "test", "Y_test.txt"))
+
+#Read the data files
+fileToDataTable <- function(f) {
+  df <- read.table(f)
+  dt <- data.table(df)
+}
+dtTrain <- fileToDataTable(file.path(pathIn, "train", "X_train.txt"))
+dtTest <- fileToDataTable(file.path(pathIn, "test", "X_test.txt"))
+
+#link data tables
+dtSubject <- rbind(dtSubjectTrain, dtSubjectTest)
+setnames(dtSubject, "V1", "subject")
+dtActivity <- rbind(dtActivityTrain, dtActivityTest)
+setnames(dtActivity, "V1", "activityNum")
+dt <- rbind(dtTrain, dtTest)
+
+#Merge columns
+
+dtSubject <- cbind(dtSubject, dtActivity)
+dt <- cbind(dtSubject, dt)
+
+#set key
+
+setkey(dt, subject, activityNum)
+
+
+
+## Extract only the mean and standard deviation
+  # Read the features.txt file
+dtFeatures <- fread(file.path(pathIn, "features.txt"))
+setnames(dtFeatures, names(dtFeatures), c("featureNum", "featureName"))
+  #subset
+
+dtFeatures <- dtFeatures[grepl("mean\\(\\)|std\\(\\)", featureName)]
+
+  # Convert the column numbers to a vector of variable names matching columns in dt.
+
+dtFeatures$featureCode <- dtFeatures[, paste0("V", featureNum)]
+head(dtFeatures)
+
+dtFeatures$featureCode
+
+select <- c(key(dt), dtFeatures$featureCode)
+dt <- dt[, select, with = FALSE]
+
+##Use descriptive activity names
+
+dtActivityNames <- fread(file.path(pathIn, "activity_labels.txt"))
+setnames(dtActivityNames, names(dtActivityNames), c("activityNum", "activityName"))
+
+
+##Label with descriptive activity names
+
+  # merge labels
+dt <- merge(dt, dtActivityNames, by = "activityNum", all.x = TRUE)
+
+  #add key
+setkey(dt, subject, activityNum, activityName)
+
+  #change data table shape
+dt <- data.table(melt(dt, key(dt), variable.name = "featureCode"))
+
+  #merge activity name
+dt <- merge(dt, dtFeatures[, list(featureNum, featureCode, featureName)], by = "featureCode", 
+            all.x = TRUE)
+
+  #create new variables
+dt$activity <- factor(dt$activityName)
+dt$feature <- factor(dt$featureName)
+
+  #help function
+grepthis <- function(regex) {
+  grepl(regex, dt$feature)
+}
+# # Features with two category
+n <- 2
+y <- matrix(seq(1, n), nrow = n)
+x <- matrix(c(grepthis("^t"), grepthis("^f")), ncol = nrow(y))
+dt$featDomain <- factor(x %*% y, labels = c("Time", "Freq"))
+x <- matrix(c(grepthis("Acc"), grepthis("Gyro")), ncol = nrow(y))
+dt$featInstrument <- factor(x %*% y, labels = c("Accelerometer", "Gyroscope"))
+x <- matrix(c(grepthis("BodyAcc"), grepthis("GravityAcc")), ncol = nrow(y))
+dt$featAcceleration <- factor(x %*% y, labels = c(NA, "Body", "Gravity"))
+x <- matrix(c(grepthis("mean()"), grepthis("std()")), ncol = nrow(y))
+dt$featVariable <- factor(x %*% y, labels = c("Mean", "SD"))
+ 
+ # Features with one category
+dt$featJerk <- factor(grepthis("Jerk"), labels = c(NA, "Jerk"))
+dt$featMagnitude <- factor(grepthis("Mag"), labels = c(NA, "Magnitude"))
+
+  # Features with three category
+n <- 3
+y <- matrix(seq(1, n), nrow = n)
+x <- matrix(c(grepthis("-X"), grepthis("-Y"), grepthis("-Z")), ncol = nrow(y))
+
+dt$featAxis <- factor(x %*% y, labels = c(NA, "X", "Y", "Z"))
+
+#Cross check
+r1 <- nrow(dt[, .N, by = c("feature")])
+r2 <- nrow(dt[, .N, by = c("featDomain", "featAcceleration", "featInstrument", 
+                           "featJerk", "featMagnitude", "featVariable", "featAxis")])
+r1 == r2
+
+
+##Create tidy data set
+  #Set key  
+setkey(dt, subject, activity, featDomain, featAcceleration, featInstrument, featJerk, featMagnitude, featVariable, featAxis)
+dtTidy <- dt[, list(count = .N, average = mean(value)), by = key(dt)]
+
